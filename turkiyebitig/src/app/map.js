28fc119103aa07eaa030bdc472cdef8a7501acfc
muhyilmaz <my956024@gmail.com>
2023-06-04 16:33:58 +0300

    Türkiye üzerine focuslama başarılı.

--- turkiyebitig/src/app/map.js
+++ turkiyebitig/src/app/map.js
@@ -9,42 +9,54 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 
 const MapPage = () => {
-  useEffect(() => {
-    // Harita öğelerini oluşturma
-    const map = new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-      view: new View({
-        center: [35.1683, 39.062],
-        zoom: 6,
-      }),
-    });
+    useEffect(() => {
+        // Harita öğelerini oluşturma
+        const map = new Map({
+            target: 'map',
+            layers: [
+                new TileLayer({
+                    source: new OSM(),
+                }),
+            ],
+            view: new View({
+                center: [35.1683, 39.062],
+                zoom: 6,
+                projection: 'EPSG:4326',
+            }),
+        });
 
-    // GeoJSON veri kaynağı oluşturma
-    const geojsonSource = new VectorSource({
-      url: "turkiye.geojson", // GeoJSON veri dosyasının yolunu buraya girin
-      format: new GeoJSON(),
-    });
+        // GeoJSON veri kaynağı oluşturma
+        const geojsonSource = new VectorSource({
+            url: "turkiye.geojson", // GeoJSON veri dosyasının yolunu buraya girin
+            format: new GeoJSON(),
+        });
 
-    // Sınırlar için vektör katmanı oluşturma
-    const vectorLayer = new VectorLayer({
-      source: geojsonSource,
-    });
+        // Sınırlar için vektör katmanı oluşturma
+        const vectorLayer = new VectorLayer({
+            source: geojsonSource,
+        });
 
-    // Vektör katmanını haritaya ekleme
-    map.addLayer(vectorLayer);
 
-    return () => {
-      // Harita öğelerini temizleme
-      map.setTarget(null);
-    };
-  }, []);
+        // Vektör katmanını haritaya ekleme
+        map.addLayer(vectorLayer);
 
-  return <div id="map" style={{ width: '100%', height: '400px' }}></div>;
+        // İsimleri ve yolları kaldırmak için mevcut katmanları bul
+        const layers = map.getLayers().getArray();
+
+        // Katmanları döngüyle kontrol et ve işaretleme ve etiketleme katmanlarını kaldır
+        layers.forEach(layer => {
+            if (layer.get('name') === 'Markers' || layer.get('name') === 'Labels') {
+                map.removeLayer(layer);
+            }
+        });
+
+        return () => {
+            // Harita öğelerini temizleme
+            map.setTarget(null);
+        };
+    }, []);
+
+    return <div id="map" style={{ width: '100%', height: '500px' }}></div>;
 };
 
 export default MapPage;
